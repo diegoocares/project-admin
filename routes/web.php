1608c5b578c6f6347cac4ed03a9aa582b8b067c6
Diego Ocares Cordova <diegoocares02@gmail.com>
2023-11-24 08:22:40 -0500

    add route group administracion
    Optimización de código usando un grupo de rutas

--- routes/web.php
+++ routes/web.php
@@ -4,24 +4,23 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 
 
-
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('administracion')->group(function () {
+    Route::get('/', [AdminController::class, 'index'])->name('admin.dashboard');
+    
+    Route::get('/empleados', [AdminController::class, 'obtenerEmpleados'])->name('obtenerEmpleados');
+    
+    Route::get('/actividades', [AdminController::class, 'mostrarActividades'])->name('mostrarActividades');
+    
+    Route::get('/addActividad', [AdminController::class, 'nuevaActividad'])->name('nuevaActividad');
+    Route::post('/guardarActividad', [AdminController::class, 'addActividad'])->name('guardarActividad');
+    
+    Route::get('/editarActividad/{id}', [AdminController::class, 'editarActividad'])->name('editarActividad');
+    
+    Route::get('/infoEmpleado/{id}', [AdminController::class, 'infoEmpleadoById'])->name('infoEmpleado');
+    
+    Route::get('/eliminarEmpleadoActividad/{id_empleado}-{id_actividad}', [AdminController::class, 'deleteEmpleadoActividad'])->name('eliminarEmpleadoActividad');
+    
+    Route::post('/agregarEmpleadoActividad', [AdminController::class, 'addEmpleadoActividad'])->name('agregarEmpleadoActividad');
 });
 
-Route::get('/control',[AdminController::class, 'index'])->name('admin.dashboard');
 
-Route::get('/empleados', [AdminController::class, 'obtenerEmpleados'])->name('obtenerEmpleados');
-
-Route::get('/actividades', [AdminController::class, 'mostrarActividades'])->name('mostrarActividades');
-
-Route::get('/addActividad', [AdminController::class, 'nuevaActividad'])->name('nuevaActividad');
-Route::post('/guardarActividad', [AdminController::class, 'addActividad'])->name('guardarActividad');
-
-Route::get('/editarActividad/{id}', [AdminController::class, 'editarActividad'])->name('editarActividad');
-
-Route::get('/infoEmpleado/{id}', [AdminController::class, 'infoEmpleadoById'])->name('infoEmpleado');
-
-Route::get('/eliminarEmpleadoActividad/{id_empleado}-{id_actividad}', [AdminController::class, 'deleteEmpleadoActividad'])->name('eliminarEmpleadoActividad');
-
-Route::post('/agregarEmpleadoActividad', [AdminController::class, 'addEmpleadoActividad'])->name('agregarEmpleadoActividad');
