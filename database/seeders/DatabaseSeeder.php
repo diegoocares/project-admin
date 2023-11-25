a3c352ee959cc6b6b5a69ad79cd310af03dc8029
Diego Ocares Cordova <diegoocares02@gmail.com>
2023-11-25 11:48:40 -0500

    Seeders Migration implementation
    Ahora se ejecuratán los seeders a la hora de migrar (php artisan migrate --seed)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        $this->call(RolesSeeder::class);
+        $this->call(EspecialidadesSeeder::class);
+        $this->call(EstadosSeeder::class);
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
